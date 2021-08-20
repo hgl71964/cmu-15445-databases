fbabb4cb5beea3b5a8825163da5241efbea65055
guoliang <guoliang@example.com>
2021-08-20 21:12:20 +0100

    fix partition key internal page

--- src/storage/index/b_plus_tree.cpp
+++ src/storage/index/b_plus_tree.cpp
@@ -307,8 +307,8 @@ void BPLUSTREE_TYPE::InsertIntoParent(BPlusTreePage *old_node,
     //      parent_page_node->GetMaxSize(), parent_page_node->GetSize());
 
     auto *new_parent_page_node = Split(parent_page_node);
-    //auto partition_key = new_parent_page_node->KeyAt(0); // partition key
-    auto partition_key = parent_page_node->KeyAt(parent_page_node->GetSize()-1); // partition key
+    auto partition_key = new_parent_page_node->KeyAt(0); // partition key
+    //auto partition_key = parent_page_node->KeyAt(parent_page_node->GetSize()-1); // partition key
     InsertIntoParent(parent_page_node, partition_key, 
                       new_parent_page_node, transaction);
     buffer_pool_manager_->UnpinPage(new_parent_page_node->GetPageId(), true);
