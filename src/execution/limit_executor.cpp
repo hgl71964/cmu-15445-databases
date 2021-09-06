bb3700c901ccbb2cc12822bfc19880a4f630d30d
guoliang <guoliang@example.com>
2021-09-06 19:05:00 +0100

    impl LIMITE

--- src/execution/limit_executor.cpp
+++ src/execution/limit_executor.cpp
@@ -18,11 +18,27 @@ LimitExecutor::LimitExecutor(ExecutorContext *exec_ctx, const LimitPlanNode *pla
                              std::unique_ptr<AbstractExecutor> &&child_executor)
     : AbstractExecutor(exec_ctx), plan_(plan), child_executor_(std::move(child_executor)) {}
 
-void LimitExecutor::Init() {}
+void LimitExecutor::Init() {
+  limit_cnt_ = 0;
+  offset_cnt_ = 0;
+}
 
 bool LimitExecutor::Next(Tuple *tuple, RID *rid) {
-  auto ok = child_executor_->Next(tuple, rid);
-  return ok;
+  if (limit_cnt_ >= plan_->GetLimit()) {
+    return false;
+  }
+
+  while (child_executor_->Next(tuple, rid)) {
+    if (offset_cnt_ < plan_->GetOffset()) {
+      offset_cnt_++;
+      continue;
+    }
+    if (limit_cnt_ < plan_->GetLimit()) {
+      limit_cnt_++;
+      return true;
+    }
+  }
+  return false;
 }
 
 }  // namespace bustub
